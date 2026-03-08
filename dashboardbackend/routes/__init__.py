66902619495b7643bd51735510c56b0c0bcc9359
pagadalanagendra <pagadalanagendra2003@gmail.com>
2026-03-08 14:14:24 +0530

    backendupdatecode

--- dashboardbackend/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import tank_sensor_routes, tank_config_routes
-
-__all__ = ["tank_sensor_routes", "tank_config_routes"]
--- dashboardbackend/services/__init__.py
+++
@@ -1,4 +0,0 @@
-# Services module
-from .thingspeak_auto_sync import auto_sync_service
-
-__all__ = ["auto_sync_service"]
